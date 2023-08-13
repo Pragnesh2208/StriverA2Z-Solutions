83a3f82c3c7a8959f008f3cf4c551bb2677b36f1
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2023-08-13 23:33:39 +0530

    Row With Max 1s added

---
+++ BinarySearch/Easy/Row-with-max-1s.cpp
@@ -0,0 +1,25 @@
+/*
+Time Complexity = O(NLogM)
+Space Complexity = O(1)
+*/
+
+int rowWithMax1s(vector<vector<int>> &matrix, int n, int m)
+{
+    //    Write your code here.
+    int ans , maxCnt = 0;
+    for(int i = 0 ; i < n ; i++) {
+        int left = -1 , right = matrix[i].size();
+
+        while(right > left + 1) {
+            int mid = left + (right - left) / 2;
+            if(matrix[i][mid] == 1) {
+                right = mid;
+            } else left =  mid;
+        }
+         if((m - right) > maxCnt) {
+             maxCnt = (m - right);
+             ans = i;
+         }
+    }
+    return ans;
+}
